d088f33b50be3edffa57d48a70b5b0263bd5e9a8
zhaolihang-com <625335512@qq.com>
2024-08-28 20:57:51 +0800

    java 11

--- tree_structure_provider/build.gradle.kts
+++ tree_structure_provider/build.gradle.kts
@@ -13,12 +13,15 @@ repositories {
 }
 
 java {
-  sourceCompatibility = JavaVersion.VERSION_17
+  sourceCompatibility = JavaVersion.VERSION_11
 }
 
 // See https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-  version.set("2023.3.7")
+  // version.set("2023.3.7")
+//  localPath.set("D:\\wp\\git\\intellij-sdk-code-samples\\tree_structure_provider\\libs\\ideaIC-2023.3.7")
+  localPath.set("D:\\wp\\git\\Bookmark-X\\local-repo\\ideaIC-2021.2.2")
+  instrumentCode.set(false)
 }
 
 tasks {
@@ -28,7 +31,7 @@ tasks {
 
   patchPluginXml {
     version.set("${project.version}")
-    sinceBuild.set("233")
+    sinceBuild.set("212")
     untilBuild.set("242.*")
   }
 }
